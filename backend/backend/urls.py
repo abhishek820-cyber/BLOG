118a8ef4db15f8a67ef207f92da44b3e15bdc875
Abhishek RP <rpabhishek820@gmail.com>
2026-03-22 21:24:32 +0530

    Added authentication (login & signup) with JWT

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from posts.views import PostViewSet, test_api
+from posts.views import PostViewSet, test_api,  register
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register('posts', PostViewSet)
@@ -10,4 +12,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/test/", test_api),
     path("api/", include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/register/', register),
 ]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
+from django.contrib.auth.models import User
 
 # ✅ KEEP THIS
 @api_view(["GET"])
@@ -13,3 +14,11 @@ def test_api(request):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
+
+@api_view(['POST'])
+def register(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = User.objects.create_user(username=username, password=password)
+    return Response({"message": "User created"})
